6d7f85cc7a2172f18800fd3abe562e8c9a4630e7
Adedoyin Benson <adedoyin.adedara@gmail.com>
2024-09-02 00:39:52 +0100

    The Flutter Ecommerce app

---
+++ Documents/Projects/flutter_ecommerce/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'views/home_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Ecommerce',
+    home: HomePage(),
+  ));
+}
